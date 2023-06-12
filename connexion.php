35f4d9fd262b9d6e00c14176b62d7228bd4793fa
Augustin Yvon <115154566+augustin-yvon@users.noreply.github.com>
2023-06-12 10:56:23 +0200

    Change PDO attribut

--- connexion.php
+++ connexion.php
@@ -3,7 +3,7 @@ require_once 'utilisateurs.php';
 
 session_start();
 
-$conn = new PDO('mysql:host=localhost;dbname=moduleconnexion;port=3308','root', '');
+$conn = new PDO('mysql:host=localhost:3306;dbname=augustin-yvon_moduleconnexion;','Admin', 'Admin@1394');
 
 // Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
